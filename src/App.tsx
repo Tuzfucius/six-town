c8e8b9e20d36c4158fd15a00bbe26f2a839095b3
Tzufucius <952105672@qq.com>
2026-07-11 14:32:55 +0800

    feat: 增加页面路由切换动画

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,41 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AnimatePresence, motion, useReducedMotion } from "motion/react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import LandingView from "./components/LandingView";
 import TownView from "./components/TownView";
 import TownMapView from "./components/TownMapView";
 
+function AnimatedRoutes() {
+  const location = useLocation();
+  const reduceMotion = useReducedMotion();
+  const transition = reduceMotion ? { duration: 0 } : { duration: 0.34, ease: [0.22, 1, 0.36, 1] as const };
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key={location.pathname}
+        initial={reduceMotion ? false : { opacity: 0, y: 14, scale: 0.992 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        exit={reduceMotion ? undefined : { opacity: 0, y: -8, scale: 1.004 }}
+        transition={transition}
+        className="min-h-screen"
+      >
+        <Routes location={location}>
+          <Route path="/" element={<LandingView />} />
+          <Route path="/:townId/info" element={<TownView />} />
+          <Route path="/:townId/map" element={<TownMapView />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingView />} />
-        <Route path="/:townId/info" element={<TownView />} />
-        <Route path="/:townId/map" element={<TownMapView />} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
   );
 }
